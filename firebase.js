941640a8138f0c0f4d5c75eae128e2fac335ab22
Rameen Sahoo <rameensahoo@gmail.com>
2022-02-22 22:55:06 -0800

    edited backend

--- firebase.js
+++ firebase.js
@@ -1,24 +1,25 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import { getAnalytics } from "firebase/analytics";
+
 // Follow this pattern to import other Firebase services
 // import { } from 'firebase/<service>';
 
 // TODO: Replace the following with your app's Firebase project configuration
-var firebaseConfig = {
-        apiKey: "AIzaSyAZS8HdNSMDVChrE-mTpN3gjuYVTxTR9Cc",
-        authDomain: "portfolio-7fd34.firebaseapp.com",
-        // The value of `databaseURL` depends on the location of the database
-        databaseURL: "https://Portfolio.firebaseio.com",
-        projectId: "portfolio-7fd34",
-        storageBucket: "portfolio-7fd34.appspot.com",
-        messagingSenderId: "277366674470",
-        appId: "1:277366674470:web:a544c15634bf94522cc2e7",
-        // For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
-        measurementId: "G-MEASUREMENT_ID",
-      };
+const firebaseConfig = {
+  apiKey: "AIzaSyAZS8HdNSMDVChrE-mTpN3gjuYVTxTR9Cc",
+  authDomain: "portfolio-7fd34.firebaseapp.com",
+  databaseURL: "https://portfolio-7fd34-default-rtdb.firebaseio.com",
+  projectId: "portfolio-7fd34",
+  storageBucket: "portfolio-7fd34.appspot.com",
+  messagingSenderId: "277366674470",
+  appId: "1:277366674470:web:a544c15634bf94522cc2e7",
+  measurementId: "G-2TVF3FMHSB"
+};
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const analytics = getAnalytics(app)
 
 // Get a list of cities from your database
 async function getCities(db) {
